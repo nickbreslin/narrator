7076f8bdd626f52e5e7f8450332d893b6be7d82c
Nick Breslin <nickbreslin@gmail.com>
2020-12-06 00:29:51 -0800

    Length of copy working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,56 @@
           <span class="badge badge-pill badge-primary">1</span> Length of copy
         </div>
         <div class="card card-body boxshadow">
-          <input class="form-control form-control-lg" v-model="wordCount" />
+          <div class="btn-group mb-3" role="group" aria-label="Basic example">
+            <button
+              type="button"
+              v-bind:class="{ active: this.activeCopy === 0 }"
+              @click="setActiveCopy(0)"
+              class="btn btn-secondary"
+            >
+              Word Count
+            </button>
+            <button
+              type="button"
+              v-bind:class="{ active: this.activeCopy === 1 }"
+              @click="setActiveCopy(1)"
+              class="btn btn-secondary"
+            >
+              Script
+            </button>
+          </div>
+
+          <!-- Word Count -->
+          <div class="copy-script-wrapper" v-if="this.activeCopy === 0">
+            <input
+              class="form-control form-control-lg mb-3"
+              type="number"
+              v-model="wordCount"
+            />
+          </div>
+
+          <!-- Textarea -->
+          <div class="copy-script-wrapper" v-if="this.activeCopy === 1">
+            <textarea class="form-control" rows="5" v-model="fullCopy" />
+
+            <div class="total mt-3 h3">
+              Total Word Count:
+              <span class="badge badge-primary">{{ fullWordCount }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-5">
+        <div class="h1">
+          <span class="badge badge-pill badge-primary">1</span> Words per minute
+        </div>
+        <div class="card card-body boxshadow">
+          <input
+            class="form-control form-control-lg"
+            type="number"
+            v-model="wordCount"
+          />
           <button class="btn btn-primary">{{ wordCount }}</button>
         </div>
         <div class="card card-body boxshadow">
@@ -40,8 +89,14 @@ export default {
   data() {
     return {
       wordCount: "rere",
-      fullCopy: ""
+      fullCopy: "",
+      activeCopy: 1
     };
+  },
+  methods: {
+    setActiveCopy(id) {
+      this.activeCopy = id;
+    }
   },
   computed: {
     fullWordCount() {
